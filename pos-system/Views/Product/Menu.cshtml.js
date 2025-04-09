25ef4023a9231b3d7149f7db5fad17a91729f3e8
Fahrizal <m.fahrizalalipradana@gmail.com>
2025-04-09 16:33:20 +0700

    calculate price

--- pos-system/Views/Product/Menu.cshtml.js
+++ pos-system/Views/Product/Menu.cshtml.js
@@ -8,83 +8,90 @@
         }).fail(function () {
             $('#productModalBody').html('<div class="alert alert-danger">Failed to load product details</div>');
         });
+
+        var product = products.find(p => p.id == productId);
+
+        if (product.price && product.price > 0) {
+            menuPrice = product.price;
+            $('#priceContainer').text(' (Rp' + menuPrice + ')');
+        }
+        else
+            menuPrice = 0;
     });
 
     $('#product-detail-close').click(function () {
         $('#productDetailModal').modal('hide');
         $('#priceContainer').text('');
+        var input = $("input[name='quant[1]']");
+        input.val(1).trigger('change');
         selected = {};
+        menuPrice = 0;
     })
 });
 
+$(document).ready(function () {
+    $('.input-number').each(function () {
+        updateButtonState($(this));
+    });
+});
 
-
-
-
-$('.btn-number').click(function (e) {
+$(document).on('click', '.btn-number', function (e) {
     e.preventDefault();
 
-    fieldName = $(this).attr('data-field');
-    type = $(this).attr('data-type');
+    var fieldName = $(this).data('field');
+    var type = $(this).data('type');
     var input = $("input[name='" + fieldName + "']");
-    var currentVal = parseInt(input.val());
-    if (!isNaN(currentVal)) {
-        if (type == 'minus') {
+    var currentVal = parseInt(input.val()) || 0;
+    var minValue = parseInt(input.attr('min')) || 0;
+    var maxValue = parseInt(input.attr('max')) || Infinity;
 
-            if (currentVal > input.attr('min')) {
-                input.val(currentVal - 1).change();
-            }
-            if (parseInt(input.val()) == input.attr('min')) {
-                $(this).attr('disabled', true);
-            }
-
-        } else if (type == 'plus') {
-
-            if (currentVal < input.attr('max')) {
-                input.val(currentVal + 1).change();
-            }
-            if (parseInt(input.val()) == input.attr('max')) {
-                $(this).attr('disabled', true);
-            }
-
+    if (type === 'minus') {
+        if (currentVal > minValue) {
+            input.val(currentVal - 1).trigger('change');
         }
-    } else {
-        input.val(0);
+    } else if (type === 'plus') {
+        if (currentVal < maxValue) {
+            input.val(currentVal + 1).trigger('change');
+        }
     }
+
+    updateButtonState(input);
 });
-$('.input-number').focusin(function () {
-    $(this).data('oldValue', $(this).val());
-});
-$('.input-number').change(function () {
 
-    minValue = parseInt($(this).attr('min'));
-    maxValue = parseInt($(this).attr('max'));
-    valueCurrent = parseInt($(this).val());
+function updateButtonState(input) {
+    var currentVal = parseInt(input.val()) || 0;
+    var minValue = parseInt(input.attr('min')) || 0;
+    var maxValue = parseInt(input.attr('max')) || Infinity;
 
-    name = $(this).attr('name');
-    if (valueCurrent >= minValue) {
-        $(".btn-number[data-type='minus'][data-field='" + name + "']").removeAttr('disabled')
-    } else {
-        $(this).val($(this).data('oldValue'));
-    }
-    if (valueCurrent <= maxValue) {
-        $(".btn-number[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
-    } else {
-        alert('Sorry, the maximum value was reached');
-        $(this).val($(this).data('oldValue'));
+    var minusBtn = $('.btn-number[data-type="minus"]');
+    var plusBtn = $('.btn-number[data-type="plus"]');
+    
+    if (currentVal <= minValue)
+        minusBtn.prop('disabled', true);
+    else
+        minusBtn.removeAttr("disabled");
+
+    if (currentVal >= maxValue)
+        plusBtn.prop('disabled', true);
+    else
+        plusBtn.removeAttr("disabled");
+
+    var totalPrice = currentVal * menuPrice;
+    $('#priceContainer').text(' (Rp' + totalPrice + ')');
+}
+
+$('.input-number').on('change', function () {
+    var valueCurrent = parseInt($(this).val()) || 0;
+    var minValue = parseInt($(this).attr('min')) || 0;
+    var maxValue = parseInt($(this).attr('max')) || Infinity;
+
+    if (valueCurrent < minValue) {
+        $(this).val(minValue);
+    } else if (valueCurrent > maxValue) {
+        $(this).val(maxValue);
     }
 
-
-});
-$(".input-number").keydown(function (e) {
-    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
-        (e.keyCode == 65 && e.ctrlKey === true) ||
-        (e.keyCode >= 35 && e.keyCode <= 39)) {
-        return;
-    }
-    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-        e.preventDefault();
-    }
+    updateButtonState($(this));
 });
 
 
