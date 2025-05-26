8c95436033ca0b797b338b00645a53d1e7d6f2ca
jesen wijaya <jwijaya476@gmail.com>
2025-05-26 20:50:12 +0700

    menambah beberapa komponen di sidebar, menambah tampilan list inventory , addinventory, InOut,dan history Inventory

---
+++ pos-system/Models/InventoryViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace pos_system.Models
+{
+    public class InventoryViewModel
+    {
+        public int Id { get; set; }
+        public string NamaBarang { get; set; }
+        public string Kategori { get; set; }
+        public string Unit { get; set; }
+        public decimal Harga { get; set; }
+        public string Note { get; set; }
+    }
+
+    public class InventoryLogViewModel
+    {
+        public string NamaBarang { get; set; }
+        public string Kategori { get; set; }
+        public int StockIn { get; set; }
+        public int StockOut { get; set; }
+        public string Note { get; set; }
+        public string InputedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+
+}
