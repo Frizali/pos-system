d1143803ecdad4c072a674efd6da0e650659d1c5
chlsnyhmhrn <chelseaanayah85@gmail.com>
2025-06-02 21:55:07 +0700

    Edit inventory and add manage account

---
+++ pos-system/Controllers/ManageAccController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pos_system.Models;
+
+public class ManageAccController : Controller
+{
+    public IActionResult ManageAcc()
+    {
+        var model = new ManageAccFormModel
+        {
+            Email = "kasir1@example.com",
+            Name = "Kasir1",
+        };
+
+        return View("~/Views/User/ManageAcc.cshtml", model);
+    }
+}
