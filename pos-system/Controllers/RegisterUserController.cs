46011a6f1bc0e12affda92f5c4cb598d14f1d7b1
chlsnyhmhrn <chelseaanayah85@gmail.com>
2025-06-10 00:43:28 +0700

    add register user page and update login page

---
+++ pos-system/Controllers/RegisterUserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pos_system.Models;
+
+public class RegisterUserController : Controller
+{
+    [HttpGet]
+    public IActionResult RegisterUser()
+    {
+        return View("~/Views/Auth/RegisterUser.cshtml");
+    }
+
+    [HttpPost]
+    public IActionResult Index(RegisterUserModel model)
+    {
+        // Dummy response 
+        ViewBag.Message = "Form submitted!";
+        ViewBag.MessageType = "success";
+
+        return View("~/Views/Auth/RegisterUser.cshtml", model);
+    }
+}
