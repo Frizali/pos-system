2622e9eb316d64dd45e14152d9a0c15fe7d91f6f
Frizali <m.fahrizalalipradana@gmail.com>
2025-07-17 17:06:36 +0700

    update url report project

--- pos-system/Services/ReportService.cs
+++ pos-system/Services/ReportService.cs
@@ -11,7 +11,7 @@ namespace pos_system.Services
         {
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("https://localhost:44339/");
+                client.BaseAddress = new Uri("http://192.168.0.105:5001/");
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
