ef29fce7b6f6f8e869e1cf6764de377dc28fc34d
Frizali <m.fahrizalalipradana@gmail.com>
2025-07-19 22:32:39 +0700

    .

---
+++ pos-system/DTOs/OrderDescDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace pos_system.DTOs
+{
+    public class OrderDescDTO
+    {
+        public string? OrderId { get; set; }
+        public string  Username { get; set; }
+        public string Email { get; set; }
+        public DateTime? ScheduledAt { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<OrderItemDecsDTO> items { get; set; } = new List<OrderItemDecsDTO>();
+    }
+
+    public class OrderItemDecsDTO
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+    }
+}
